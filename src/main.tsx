e91262afad8a2daa222f3477afd4251caefa6581
Jeremy Odell <jeremy.odell01@gmail.com>
2023-11-14 18:30:52 -0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-// routing
-const basename = "/revelio-oa/";
-
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={basename}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
